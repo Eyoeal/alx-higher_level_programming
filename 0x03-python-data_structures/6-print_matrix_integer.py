af70d702872ca2512d99b5bbe3c38b998fdb2f39
Eyoeal <novamax418@gmail.com>
2021-11-22 13:20:16 -0800

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Function that prints a matrix of integers.
+    Args:
+        matrix: matrix to be printed
+    Returns:
+        Nothing
+    """
+    if not len(matrix[0]) and len(matrix) == 1:
+        print("")
+    for row in matrix:
+        for element in row:
+            if row.index(element) == len(row) - 1:
+                print("{:d}".format(element))
+                continue
+            print("{:d}".format(element), end=" ")
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """Function that finds the biggest integer of a list.
+    Args:
+        my_list: the list to be scanned
+    Returns:
+        Max value in a list
+    """
+    if len(my_list) == 0:
+        return None
+    max = 0
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
